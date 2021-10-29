8136d9754fd3797bd0d9a98b991a284c1b634b79
Jorge Server <jorge.montagut@gmail.com>
2021-10-28 19:19:46 -0500

    first commit

---
+++ src/main/java/com/tiendis/tiendis/TiendisApplication.java
@@ -0,0 +1,13 @@
+package com.tiendis.tiendis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tiendis/tiendis/TiendisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tiendis.tiendis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
