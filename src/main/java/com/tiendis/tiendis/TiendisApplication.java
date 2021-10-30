259ac08d6a23c56ac293a78dc8ceebaa4d8cc942
Jorge Server <jorge.montagut@gmail.com>
2021-10-30 18:22:30 -0500

    guardar datos

--- src/main/java/com/tiendis/tiendis/TiendisApplication.java
+++ src/main/java/com/tiendis/tiendis/TiendisApplication.java
@@ -2,6 +2,9 @@ package com.tiendis.tiendis;
 
 import com.tiendis.tiendis.Repository.CategoriaRepository;
 import com.tiendis.tiendis.entity.Categoria;
+import org.apache.juli.logging.Log;
+import org.apache.juli.logging.LogFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,22 +13,39 @@ import java.util.List;
 
 
 @SpringBootApplication
-public class TiendisApplication {
+public class TiendisApplication implements CommandLineRunner {
 
 	private CategoriaRepository categoriaRepository;
+
+	public TiendisApplication(CategoriaRepository categoriaRepository) {
+		this.categoriaRepository = categoriaRepository;
+	}
+
+	private final Log LOGGER = LogFactory.getLog(TiendisApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(TiendisApplication.class, args);
 	}
-
 
 	private void saveCategoryInDataBase(){
 		Categoria lapices = new Categoria("lapices", "Lapices varios");
 		Categoria lapiceros = new Categoria("lapiceros", "lapiceros");
 		Categoria cuadernos = new Categoria("cuadernos", "cuadernos grapado, argollados, cocidos");
 		List<Categoria> list = Arrays.asList(lapices, lapiceros, cuadernos);
+		System.out.print("listando articuos antes de guardar");
 		list.stream().forEach(categoriaRepository::save);
 
+
+
+	}
+	@Override
+	public void run(String... args) {
+		try {
+			saveCategoryInDataBase();
+		} catch (Exception e){
+			e.printStackTrace ();
+			LOGGER.error("Esto es un error" + e.getMessage());
+		}
 	}
 
 }
